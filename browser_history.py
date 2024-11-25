354a1fc70020518c38c9815aa053984c7afdfc98
Vivek Silimkhan <viveksilimkhan@gmail.com>
2024-11-26 00:06:02 +0530

    Add browser history member code

---
+++ browser_history.py
@@ -0,0 +1,195 @@
+import os
+import sqlite3
+import platform
+from datetime import datetime, UTC, timedelta
+
+
+def fetch_safari_history() -> List[Dict]:
+    if platform.system() != "Darwin":
+        return []
+    safari_db_path = os.path.expanduser("~/Library/Safari/History.db")
+    if not os.path.exists(safari_db_path):
+        print("Safari history database not found.")
+        return []
+
+    # Copying is necessary because databases are locked
+    # Copies are intentionally outside syftbox, but we can process them locally
+    temp_db_path = Path("~/.tmp/Safary_History.db").expanduser()
+    shutil.copy(safari_db_path, temp_db_path)
+
+    conn = sqlite3.connect(temp_db_path)
+    cursor = conn.cursor()
+    cursor.execute("""
+        SELECT 
+            history_items.url, 
+            history_visits.visit_time 
+        FROM 
+            history_visits 
+        JOIN 
+            history_items 
+        ON 
+            history_items.id = history_visits.history_item
+        ORDER BY 
+            visit_time DESC
+    """)
+    rows = cursor.fetchall()
+    conn.close()
+
+    # Remove tmp copy as we are done with it
+    temp_db_path.unlink(missing_ok=True)
+
+    history = []
+    for url, visit_time in rows:
+        visit_time = datetime(2001, 1, 1) + timedelta(seconds=visit_time)
+        history.append({"url": url, "visit_time": visit_time, "browser":"safari"})
+    return history
+
+
+def fetch_chrome_history() -> List[Dict]:
+    db_paths = {
+        "Darwin": os.path.expanduser("~/Library/Application Support/Google/Chrome/Default/History"),
+        "Linux": os.path.expanduser("~/.config/google-chrome/Default/History")
+    }
+    chrome_db_path = db_paths.get(platform.system())
+    if not chrome_db_path or not os.path.exists(chrome_db_path):
+        print("Chrome history database not found.")
+        return []
+    
+    # Copying is necessary because databases are locked
+    # Copies are intentionally outside syftbox, but we can process them locally
+    temp_db_path = Path("~/.tmp/Chrome_History").expanduser()
+    shutil.copy(chrome_db_path, temp_db_path)
+     
+    conn = sqlite3.connect(temp_db_path)
+    cursor = conn.cursor()
+    cursor.execute("""
+        SELECT 
+            urls.url, 
+            urls.last_visit_time 
+        FROM 
+            urls 
+        ORDER BY 
+            last_visit_time DESC
+    """)
+    rows = cursor.fetchall()
+    conn.close()
+    
+    temp_db_path.unlink(missing_ok=True)
+
+    history = []
+    for url, last_visit_time in rows:
+        visit_time = datetime(1601, 1, 1) + timedelta(microseconds=last_visit_time)
+        history.append({"url": url, "visit_time": visit_time, "browser":"chrome"})
+    return history
+
+
+def fetch_firefox_history() -> List[Dict]:
+    db_paths = {
+        "Darwin": os.path.expanduser("~/Library/Application Support/Firefox/Profiles"),
+        "Linux": os.path.expanduser("~/.mozilla/firefox")
+    }
+    firefox_profile_path = db_paths.get(platform.system())
+    if not firefox_profile_path or not os.path.exists(firefox_profile_path):
+        print("Firefox profile directory not found.")
+        return []
+    
+    history = []
+    for profile in os.listdir(firefox_profile_path):
+        profile_path = os.path.join(firefox_profile_path, profile)
+        if not os.path.isdir(profile_path):
+            continue
+        places_db = os.path.join(profile_path, "places.sqlite")
+
+        if not os.path.exists(places_db):
+            continue
+
+        # Copying is necessary because databases are locked
+        # Copies are intentionally outside syftbox, but we can process them locally
+        temp_db_path = Path("~/.tmp/Firefox_places.sqlite").expanduser()
+        shutil.copy(places_db, temp_db_path)
+     
+        conn = sqlite3.connect(temp_db_path)
+        cursor = conn.cursor()
+        cursor.execute("""
+            SELECT 
+                moz_places.url, 
+                moz_historyvisits.visit_date 
+            FROM 
+                moz_places 
+            JOIN 
+                moz_historyvisits 
+            ON 
+                moz_places.id = moz_historyvisits.place_id
+            ORDER BY 
+                visit_date DESC
+        """)
+        rows = cursor.fetchall()
+        conn.close()
+        
+        temp_db_path.unlink(missing_ok=True)
+
+        for url, visit_date in rows:
+            visit_time = datetime(1970, 1, 1) + timedelta(microseconds=visit_date)
+            history.append({"url": url, "visit_time": visit_time, "browser":"firefox"})
+    return history
+
+def fetch_brave_history() -> List[Dict]:
+    db_paths = {
+        "Darwin": os.path.expanduser("~/Library/Application Support/BraveSoftware/Brave-Browser/Default/History"),
+        "Linux": os.path.expanduser("~/.config/BraveSoftware/Brave-Browser/Default/History")
+    }
+
+    brave_profile_path = db_paths.get(platform.system())
+    if not brave_profile_path or not os.path.exists(brave_profile_path):
+        print("brave profile directory not found.")
+        return []
+
+    # Copying is necessary because databases are locked
+    # Copies are intentionally outside syftbox, but we can process them locally
+    temp_db_path = Path("~/.tmp/brave_places.sqlite").expanduser()
+    shutil.copy(brave_profile_path, temp_db_path)
+    
+    conn = sqlite3.connect(temp_db_path)
+    cursor = conn.cursor()
+    cursor.execute("""
+        SELECT url,last_visit_time FROM urls ORDER BY last_visit_time DESC
+    """)
+    rows = cursor.fetchall()
+    conn.close()
+    ""
+    temp_db_path.unlink(missing_ok=True)
+    history = []
+    print(rows)
+    for data in rows:
+        (url, visit_time) = data 
+        visit_time = datetime(1601, 1, 1) + timedelta(microseconds=visit_time)
+        history.append({"url": url, "visit_time": visit_time, "browser":"brave"})
+    return history
+
+def fetch_combined_history() -> List[Dict]:
+    temp_folder = Path("~/.tmp").expanduser()
+    os.makedirs(temp_folder, exist_ok=True)
+
+    print("Fetching Safari history...")
+    safari_history = fetch_safari_history()
+    print(f"Safari history: {len(safari_history)} items")
+
+    print("\nFetching Chrome history...")
+    chrome_history = fetch_chrome_history()
+    print(f"Chrome history: {len(chrome_history)} items")
+
+    print("\nFetching Firefox history...")
+    firefox_history = fetch_firefox_history()
+    print(f"Firefox history: {len(firefox_history)} items")
+
+
+    print("\nFetching Brave history...")
+    brave_history = fetch_brave_history()
+    print(f"Brave history: {len(brave_history)} items")
+
+    print("\nSafari sample history:", safari_history[:5])
+    print("Chrome sample history:", chrome_history[:5])
+    print("Firefox sample history:", firefox_history[:5])
+    print("Brave sample history:", brave_history[:5])
+    combined_history = safari_history + chrome_history + firefox_history + brave_history
+    return combined_history
---
+++ main.py
@@ -0,0 +1,142 @@
+import os
+from pathlib import Path
+import json
+from syftbox.lib import Client, SyftPermission
+import diffprivlib.tools as dp
+import time
+import psutil
+from statistics import mean
+from datetime import datetime, UTC
+import sqlite3
+import platform
+from datetime import datetime, timedelta
+from typing import List, Dict
+import shutil
+from urllib.parse import urlparse, parse_qs
+from browser_history import *
+
+API_NAME = "browser_history_member"
+AGGREGATOR_DATASITE = "aggregator@openmined.org"
+
+def split_url(url:List[str], private:bool = False):
+    try:
+        parsed_url = urlparse(url)
+        components = {
+            "scheme": parsed_url.scheme,
+            "netloc": parsed_url.netloc,
+            "path": parsed_url.path,
+            "params": parsed_url.params,
+            "query": parsed_url.query,
+            "fragment": parsed_url.fragment,
+        }
+        if private:
+            if parsed_url.query:
+                components["query_params"] = parse_qs(parsed_url.query)
+
+        return components
+    except Exception as e:
+        return {"error": str(e)}
+
+
+def create_restricted_public_folder(browser_history_path: Path) -> None:
+    """
+    Create an output folder for browser history data within the specified path.
+
+    This function creates a directory structure for storing browser history data under `api_data/browser_history`. If the directory
+    already exists, it will not be recreated. Additionally, default permissions for accessing the created folder are set using the
+    `SyftPermission` mechanism to allow the data to be read by an aggregator.
+
+    Args:
+        path (Path): The base path where the output folder should be created.
+
+    """
+    os.makedirs(browser_history_path, exist_ok=True)
+
+    # Set default permissions for the created folder
+    permissions = SyftPermission.datasite_default(email=client.email)
+    permissions.read.append(AGGREGATOR_DATASITE)
+    permissions.save(browser_history_path)
+
+
+def create_private_folder(path: Path) -> Path:
+    """
+    Create a private folder for browser history data within the specified path.
+
+    This function creates a directory structure for storing browser history data under `private/browser_history`.
+    If the directory already exists, it will not be recreated. Additionally, default permissions for
+    accessing the created folder are set using the `SyftPermission` mechanism, allowing the data to be
+    accessible only by the owner's email.
+
+    Args:
+        path (Path): The base path where the output folder should be created.
+
+    Returns:
+        Path: The path to the created `browser_history` directory.
+    """
+    browser_history_path: Path = path / "private" / "browser_history"
+    os.makedirs(browser_history_path, exist_ok=True)
+
+    # Set default permissions for the created folder
+    permissions = SyftPermission.datasite_default(email=client.email)
+    permissions.save(browser_history_path)
+
+    return browser_history_path
+
+
+def save(path: str, browser_history: List[Dict]):
+    current_time = datetime.now(UTC)
+    timestamp_str = current_time.strftime("%Y-%m-%d %H:%M:%S")
+
+    with open(path, "w") as json_file:
+        json.dump(
+            {"browser_history": browser_history, "timestamp": timestamp_str},
+            json_file,
+            indent=4,
+        )
+
+
+def should_run() -> bool:
+    INTERVAL = 20  # 20 seconds
+    timestamp_file = f"./script_timestamps/{API_NAME}_last_run"
+    os.makedirs(os.path.dirname(timestamp_file), exist_ok=True)
+    now = datetime.now().timestamp()
+    time_diff = INTERVAL  # default to running if no file exists
+    if os.path.exists(timestamp_file):
+        try:
+            with open(timestamp_file, "r") as f:
+                last_run = int(f.read().strip())
+                time_diff = now - last_run
+        except (FileNotFoundError, ValueError):
+            print(f"Unable to read timestamp file: {timestamp_file}")
+    if time_diff >= INTERVAL:
+        with open(timestamp_file, "w") as f:
+            f.write(f"{int(now)}")
+        return True
+    return False
+   
+
+if __name__ == "__main__":
+    if not should_run():
+        print(f"Skipping {API_NAME}, not enough time has passed.")
+        exit(0)
+
+    client = Client.load()
+
+    # Create an output file with proper read permissions
+    restricted_public_folder = client.api_data("browser_history")
+    create_restricted_public_folder(restricted_public_folder)
+
+    # Create private folder
+    private_folder = create_private_folder(client.datasite_path)
+
+    # Get browser data
+    combined_history = fetch_combined_history()
+    browser_history_private, browser_history_public = [split_url(urlstr["url"]) for urlstr in combined_history], [split_url(urlstr["url"], private=True) for urlstr in combined_history]
+
+    # Saving public browser history added in it.
+    public_file: Path = restricted_public_folder / "browser_history.json"
+    save(path=str(public_file), browser_history=browser_history_public)
+
+    # Saving the private browser history.
+    private_file: Path = private_folder / "browser_history.json"
+    save(path=str(private_file), browser_history=browser_history_private)
