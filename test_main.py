33ed6ef2c195b0abbfd1170edf695f2c4f8fed79
Vivek Silimkhan <viveksilimkhan@gmail.com>
2024-11-26 00:53:00 +0530

    Add scheme stats and minor fix

--- test_main.py
+++ test_main.py
@@ -36,22 +36,30 @@ def split_url(url:List[str], private:bool = False):
     except Exception as e:
         return {"error": str(e)}
 
-def save(path: str, browser_history: List[Dict]):
+def save(path: str, browser_history: List[Dict], mode: str = "all"):
     current_time = datetime.utcnow()
     timestamp_str = current_time.strftime("%Y-%m-%d %H:%M:%S")
-
-    with open(path, "w") as json_file:
-        json.dump(
-            {"browser_history": browser_history, "timestamp": timestamp_str},
-            json_file,
-            indent=4,
-        )
-
+    if mode == "all":
+        with open(path, "w") as json_file:
+            json.dump(
+                {"browser_history": browser_history, "timestamp": timestamp_str},
+                json_file,
+                indent=4,
+            )
+    elif mode == "scheme":
+        with open(path, "w") as json_file:
+            json.dump(
+                {"scheme_avg": len([bh["scheme"] for bh in browser_history if bh["scheme"] == "https"])c/len(browser_history), "timestamp": timestamp_str},
+                json_file,
+                indent=4,
+            )
 
 if __name__ == "__main__":
 
     # Get browser data
     combined_history = fetch_combined_history()
     browser_history_private, browser_history_public = [split_url(urlstr["url"]) for urlstr in combined_history], [split_url(urlstr["url"], private=True) for urlstr in combined_history]
+
     save("./browser_history_private.json",browser_history_private)
-    save("./browser_history_public.json",browser_history_private)
+    save("./browser_history_public.json",browser_history_public)
+    save("./scheme_stats.json",browser_history_public, mode="scheme")
